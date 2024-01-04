83695e1f87dd9729a3af13a90b50fe832bedcd5b
김동우(DongWoo Kim)[gkf9876] <gkf9876@LGENSOL.GLOBAL>
2024-01-04 14:43:10 +0900

    ALLOWED_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b3imhh-pew9j6c2dvocx@o8uw5@ok1v2)a)5_8%3)9ub_b$j&i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['114.207.212.59']
 
 
 # Application definition
