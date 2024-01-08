a79021410bb10ba3193f4348d97ccd1612ae061b
김동우(DongWoo Kim)[gkf9876] <gkf9876@LGENSOL.GLOBAL>
2024-01-08 15:28:49 +0900

    도메인 적용

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['114.207.212.59']
+ALLOWED_HOSTS = ['114.207.212.59', 'gkf9876.mooo.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
