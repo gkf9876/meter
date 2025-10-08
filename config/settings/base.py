1deb9a72b781d33d515083581f9f9be44a8bb18f
gkf9876 <gkf9876@naver.com>
2025-10-08 14:19:42 +0900

    tinymce gpl 라이센스 적용

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -204,6 +204,7 @@ LOGGING = {
         },
     }
 }
+TINYMCE_JS_URL = os.path.join(STATIC_URL, 'tinymce/tinymce.min.js')
 TINYMCE_DEFAULT_CONFIG = {
     'cleanup_on_startup': True,
     'custom_undo_redo_levels': 20,
