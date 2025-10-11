8a56092bfa35f96cbd6cefd137bacc4289d5dad8
김동우 <gkf9876@naver.com>
2025-10-11 13:57:29 +0900

    todo 경로 정리

--- todo/urls.py
+++ todo/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
     path('<int:todo_id>/', base_views.detail, name='detail'),
 
     # todo_views.py
-    path('todo/create/', todo_views.todo_create, name='create'),
-    path('todo/modify/<int:todo_id>/', todo_views.todo_modify, name='modify'),
-    path('todo/delete/<int:todo_id>/', todo_views.todo_delete, name='delete'),
-    path('todo/dragdrop/', todo_views.todo_dragdrop, name='dragdrop'),
-    path('todo/check/<int:todo_id>/<str:check_yn>/', todo_views.todo_check, name='check'),
+    path('create/', todo_views.todo_create, name='create'),
+    path('modify/<int:todo_id>/', todo_views.todo_modify, name='modify'),
+    path('delete/<int:todo_id>/', todo_views.todo_delete, name='delete'),
+    path('dragdrop/', todo_views.todo_dragdrop, name='dragdrop'),
+    path('check/<int:todo_id>/<str:check_yn>/', todo_views.todo_check, name='check'),
 
     # todo_detail_views.py
-    path('tododetail/create/<int:todo_id>', todo_detail_views.tododetail_create, name='detail_create'),
-    path('tododetail/modify/<int:todo_detail_id>', todo_detail_views.tododetail_modify, name='detail_modify'),
-    path('tododetail/delete/<int:todo_detail_id>', todo_detail_views.tododetail_delete, name='detail_delete'),
+    path('detail_create/<int:todo_id>', todo_detail_views.tododetail_create, name='detail_create'),
+    path('detail_modify/<int:todo_detail_id>', todo_detail_views.tododetail_modify, name='detail_modify'),
+    path('detail_delete/<int:todo_detail_id>', todo_detail_views.tododetail_delete, name='detail_delete'),
 
 ]
