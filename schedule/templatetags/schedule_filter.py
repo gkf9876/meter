d86afed9cf36bed50a054338cdec6a99684f00b1
김동우 <gkf9876@naver.com>
2025-10-14 21:08:24 +0900

    시간 변환 필터 생성하여 처리

--- schedule/templatetags/schedule_filter.py
+++ schedule/templatetags/schedule_filter.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import time, datetime
 
 register = template.Library()
 
@@ -9,6 +10,15 @@ def sub(value, arg):
 @register.filter
 def filter_useYn(list, use_yn):
     return list.filter(use_yn=use_yn)
+
 @register.filter
 def div(value, arg):
     return value / arg
+
+@register.filter
+def smart_time(value):
+    if not value:
+        return ''
+    if isinstance(value, (time, datetime)):
+        return value.strftime('%H:%M')
+    return str(value)
