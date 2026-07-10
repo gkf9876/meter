5164c80c54e2bfefc547b72c567ba691ce889e87
gkf9876 <gkf9876@naver.com>
2026-07-10 23:24:55 +0900

    ip 가져오는 방식 변경

--- accounts/signals.py
+++ accounts/signals.py
@@ -14,7 +14,16 @@ def save_login_log(sender, request, user, **kwargs):
     LoginLog.objects.create(
         user=user,
         username=user.username,
-        ip=ip,
+        ip=get_client_ip(request),
         user_agent=user_agent,
         success=True
     )
+
+def get_client_ip(request):
+
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+
+    if x_forwarded_for:
+        return x_forwarded_for.split(",")[0].strip()
+
+    return request.META.get("REMOTE_ADDR")
